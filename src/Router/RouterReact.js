d37f0f1334cfb8acedc4ea654e9168095d5e1ec3
Przemyslaw-Swiderski <przemyslaw.rafal.swiderski@gmail.com>
2023-09-18 14:42:46 +0200

    next development

--- src/Router/RouterReact.js
+++ src/Router/RouterReact.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import ProductsPage from "./ProductsPage";
 import Contact from "./Contact";
+import LoginForm from "../RegistrationAndLogin/LoginForm";
+import RegistrationForm from "../RegistrationAndLogin/RegistrationForm";
 import { Container } from '@mui/material';
 
 const containerStyle = {
@@ -18,6 +20,8 @@ function RouterReact () {
             <Route path="/" element={<ProductsPage />} />
             <Route path="/home" element={<ProductsPage/>}/>
             <Route path="/contact" element={<Contact/>}/>
+            <Route path="/login" element={<LoginForm/>}/>
+            <Route path="/register" element={<RegistrationForm/>}/>
         </Routes>
       </Router>
       </Container>
